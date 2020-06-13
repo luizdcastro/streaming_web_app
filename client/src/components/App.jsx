c2bfe379be5a7c4cc1097207e671bbb31947b5c0
Luiz Castro <lfernando.cwb@gmail.com>
2020-06-13 18:07:02 -0300

    added routes, header and components boilerplate

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,7 +1,27 @@
 import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+import Header from "../components/header/header.component";
+import StreamCreate from "../components/streams/stream-create.component";
+import StreamEdit from "../components/streams/stream-edit.component";
+import StreamList from "../components/streams/stream-list.component";
+import StreamShow from "../components/streams/stream-show.component";
+import StreamDelete from "../components/streams/stream-delete.component";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <BrowserRouter>
+        <div>
+          <Header className="ui container" />
+          <Route path="/" exact component={StreamList} />
+          <Route path="/streams/new" exact component={StreamCreate} />
+          <Route path="/streams/edit" exact component={StreamEdit} />
+          <Route path="/streams/delete" exact component={StreamDelete} />
+          <Route path="/streams/show" exact component={StreamShow} />
+        </div>
+      </BrowserRouter>
+    </div>
+  );
 };
 
 export default App;
